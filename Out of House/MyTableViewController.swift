8a970c12ee9957893aab10f168e022cfd00dfd1e
bo5284 <bo5284@naver.com>
2018-01-19 15:35:02 +0900

    수정 테이블 추가

--- Out of House/MyTableViewController.swift
+++ Out of House/MyTableViewController.swift
@@ -35,6 +35,11 @@ class MyTableViewController: UITableViewController {
     ]
     
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Alarmcell", for: indexPath)
         
